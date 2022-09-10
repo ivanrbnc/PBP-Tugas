fd3f76a9bd159c552f8a3f7d78b8391b7648579d
Ivan Rabbani Cezeliano <ivanrabbanicool@gmail.com>
2022-09-10 23:32:14 +0700

    Update 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Ivan Rabbani Cezeliano',
+    'npm' : '2106701892'
+}
